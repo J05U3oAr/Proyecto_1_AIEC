86935cf61b52cc20a2035c765c97bc5a32e3e029
J05U3oAr <chaarodi@gmail.com>
2026-09-23 10:44:04 -0600

    fix: preparar streams web para pruebas de MSW

--- src/test/setupTests.ts
+++ src/test/setupTests.ts
@@ -1,6 +1,9 @@
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
+import { ReadableStream, TransformStream, WritableStream } from 'node:stream/web';
 import { afterEach } from 'vitest';
+
+Object.assign(globalThis, { ReadableStream, TransformStream, WritableStream });
 
 // Automatically unmount and cleanup DOM after the test is finished.
 afterEach(() => {
